3e433ffc96a76abf1216ea66d7d229cdf4c12262
Hubert Podgórski <hubert.podgorski95@gmail.com>
2024-01-03 19:04:06 +0100

    temporarily disable notifications

--- src/socketRoutes/notifications.js
+++ src/socketRoutes/notifications.js
@@ -7,17 +7,17 @@ const {
 const routes = (io, socket) => {
   const userToken = socket.handshake.query.token;
 
-  socket.on("save_subscription", (received, callback) =>
-    saveSubscription(received, callback, userToken)
-  );
+  socket.on("save_subscription", (received, callback) => {
+    // saveSubscription(received, callback, userToken);
+  });
 
-  socket.on("send_notification", (received) =>
-    sendNotification(received, userToken)
-  );
+  socket.on("send_notification", (received) => {
+    // sendNotification(received, userToken);
+  });
 
-  socket.on("get_subscription_details", (callback) =>
-    getSubscriptionDetails(callback, userToken)
-  );
+  socket.on("get_subscription_details", (callback) => {
+    // getSubscriptionDetails(callback, userToken);
+  });
 };
 
 module.exports = routes;
